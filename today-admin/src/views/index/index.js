9c264b714a6da1bd826ea68d2272a9360c69b14a
liupingtoday <liupingtoday@163.com>
2020-01-03 23:16:49 +0800

    添加首页的demo内容

--- today-admin/src/views/index/index.js
+++ today-admin/src/views/index/index.js
@@ -1,3 +1,7 @@
 import '../../assets/Common';
+import '../../assets/css/bootstrap_limitless.css';
+import '../../assets/css/colors.css';
+import '../../assets/css/components.css';
+import '../../assets/css/layout.css';
 import './index.scss';
 import './index.html';
